a15b2b017622cb26013c0ae8d87362fa8a3b7cc1
Noppanut Ploywong <noppanut15@hotmail.com>
2016-10-24 11:33:38 +0700

    Add backend section.

---
+++ PokeEZ/urls.py
@@ -0,0 +1,14 @@
+"""PokeEZ URL Configuration"""
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^pokedex/', views.pokedex, name='pokedex'),
+    url(r'^about-us/', views.about, name='about'),
+    url(r'^analytic/', include('analytic.urls')),
+    url(r'^search-by-name/', views.search_by_name, name='search'),
+
+]
