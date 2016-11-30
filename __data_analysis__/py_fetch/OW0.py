5fb1cdad04f097b43ca80cf6d412128a9317254a
Apichaya Kampo <59070186@it.kmitl.ac.th>
2016-12-01 04:00:54 +0700

    Add Overview Part 2

--- __data_analysis__/py_fetch/OW0.py
+++ __data_analysis__/py_fetch/OW0.py
@@ -5,7 +5,8 @@ def fmid(num):
 data = []
 for i in range(1, 152):
     with open('../py_output_json/DA0/'+fmid(i)+'.json', 'r') as latlng_file:
-        data += json.load(latlng_file)
+        data = json.load(latlng_file)
+        print(len(data))
 
 with open('../py_output_json/DA0_map_overview.json', '+w') as f:
     line = json.dumps(data)
