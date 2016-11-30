1ba0ae813c41723f0e6cf8d7929a41bb10e3b8a8
Apichaya Kampo <59070186@it.kmitl.ac.th>
2016-12-01 00:14:40 +0700

    Add Intro section

---
+++ __data_analysis__/py_fetch/DA0_map_overview.py
@@ -0,0 +1,13 @@
+import json
+def fmid(num):
+    return '%03d' % num
+
+data = []
+for i in range(1, 152):
+    with open('../py_output_json/DA0/'+fmid(i)+'.json', 'r') as latlng_file:
+        data += json.load(latlng_file)
+
+with open('../py_output_json/DA0_map_overview.json', '+w') as f:
+    line = json.dumps(data)
+    f.write(line)
+
